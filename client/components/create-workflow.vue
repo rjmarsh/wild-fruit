7a6669763ae1ecfedcdbb8229f69cd0a1d6ac07e
Russ Marsh <rumarsh@adobe.com>
2017-03-15 17:36:46 -0600

    create-workflow form

--- client/components/create-workflow.vue
+++ client/components/create-workflow.vue
@@ -2,7 +2,48 @@
 	<div class="create-workflow">
 		<wf-modal :show="showModal" :on-close="closeModal">
 			<span slot="content">
-				<span class="cw-title">Create a New Workflow</span>
+				<div class="form-title">Create a New Workflow</div>
+				<div class="form-group">
+					<div class="column">
+						<label>New Workflow Name</label>
+						<input class="input-control" placeholder="Descendancy of Splendora Fritella"/>
+						<label>Root Person</label>
+						<input class="input-control" pattern="" placeholder="L2DE-A1Z"/>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="row">
+						<input type="radio" id="pickUp" value="Up" v-model="upDown">
+						<label for="pickUp">Ascending</label>
+						<input type="radio" id="pickDown" value="Down" v-model="upDown">
+						<label for="pickDown">Descending</label>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="row">
+						<input type="radio" id="pickDirect" value="Direct" v-model="directPicked">
+						<label for="pickDirect">Direct Line</label>
+						<input type="radio" id="pickPlus" value="Plus" v-model="directPicked">
+						<label for="pickPlus">Direct Line and Immediate Families</label>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="row">
+						<label>Generations</label>
+						<input class="input-control" placeholder="4"/>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="column">
+						<label>&nbsp;</label>
+						<button>Save</button>
+						<button>Cancel</button>
+					</div>
+				</div>
 			</span>
 		</wf-modal>
 	</div>
@@ -14,7 +55,11 @@
 	export default {
 		name: 'wf-create-workflow',
 		data() {
-			return {};
+			return {
+				workflowName: '',
+				upDown: 'Up',
+				directPicked: 'Direct',
+			};
 		},
 		props: ['showModal'],
 		components: {
@@ -35,8 +80,8 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 	.create-workflow {
-		display: table;
-		background-color: #3a3636;
+		display: flex;
+		background-color: #ffffff;
 		color: #999;
 		padding: 15px;
 		margin: 0;
@@ -54,17 +99,54 @@
 		opacity: 1;
 	}
 
-	.create-workflow a {
-		color: #ffffff;
-		text-decoration: none;
+	/* form specific formatting */
+	.form-title {
+		text-align: center;
+		padding-bottom: 20px;
 	}
 
-	.create-workflow a:hover {
-		color: #9ce;
+	.form-group {
+		border-width: thick;
+		border-color: #5a6100;
+		border-top-style: solid;
+		padding: 20px 5px;
+		margin-left: 10px;
+		margin-right: 10px;
 	}
 
-	.cw-title {
-		font-size: 1.3em;
+	.form-group .row {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.form-group .column {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.form-group label {
+		flex: 1 1 auto;
+		display: block;
+		font-weight: bold;
+		font-size: 1em;
+		padding-left: 5px;
+		padding-right: 10px;
+	}
+
+	.form-group .input-control {
+		flex: 1 1 auto;
+		display: block;
+		margin-bottom: 10px;
+		margin-right: 8px;
+		padding: 4px;
+		margin-top: 4px;
+	}
+
+	button {
+		padding: 5px 15px;
+		margin: 5px;
+		min-width: 100px
 	}
 
 </style>
--- client/views/create.vue
+++ client/views/create.vue
@@ -8,7 +8,6 @@
 	import wfCreate from '../components/create-workflow';
 
 	export default {
-		name: 'Create',
 		data() {
 			return {};
 		},
